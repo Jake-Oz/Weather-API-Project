2fdea271e40569525772d5879498d72bd9b34475
Jake Campbell <jakeandrach@me.com>
2022-06-14 17:06:32 +1000

    Weather app using APIs and NodeJS server

---
+++ index.js
@@ -0,0 +1,52 @@
+//server side software
+
+const express = require("express");
+const fetch = require("node-fetch");
+const Datastore = require("nedb");
+const app = express();
+const port = 3000;
+
+const db = new Datastore({ filename: "database" });
+
+db.loadDatabase(function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/weather/:latlon", async (request, response) => {
+  const latlong = request.params.latlon.split(",");
+  const lat = latlong[0];
+  const lon = latlong[1];
+
+  const weather_url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lat=${lat}&lon=${lon}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "59a0018805msh1af9d87852742d0p1614f9jsn5936d8d15e9e",
+      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+    },
+  };
+
+  let aq_url = `https://api.openaq.org/v2/latest?limit=100&page=1&offset=0&sort=desc&has_geo=true&coordinates=${lat}%2C${lon}&radius=20000&order_by=lastUpdated&dumpRaw=false`;
+  aq_url = aq_url.replace(/\s+/g, "");
+  db.insert({ lat: lat, long: lon });
+  const weather_response = await fetch(weather_url, options);
+  const weather_data = await weather_response.json();
+  const aq_response = await fetch(aq_url);
+  const aq_data = await aq_response.json();
+  const allData = { weather_data, aq_data };
+  response.json(allData);
+});
+
+app.get("/weatherData", (request, response) => {
+  db.find({}, function (err, docs) {
+    response.json(docs);
+  });
+});
---
+++ public/client.js
@@ -0,0 +1,75 @@
+let lat, lon;
+var map = L.map("map").setView([-30, 150], 1);
+document.getElementById("map").style.visibility = "hidden";
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    document.getElementById("lat").textContent = lat.toFixed(2);
+    document.getElementById("lon").textContent = lon.toFixed(2);
+
+    map.setView([lat, lon], 1);
+
+    const data_url = `/weather/${lat}, ${lon}`;
+    const response = await fetch(data_url);
+    const json = await response.json();
+    const weatherData = json.weather_data;
+    const air_qualityData = json.aq_data;
+    // document.getElementById("location").textContent =
+    //   weatherData.data[0].city_name;
+    // document.getElementById("summary").textContent =
+    //   weatherData.data[0].weather.description;
+    // const iconElement = document.getElementById("icon");
+    // iconElement.src = `icons/${weatherData.data[0].weather.icon}.png`;
+    // iconElement.style.width = "30px";
+    // document.getElementById("temperature").textContent =
+    //   weatherData.data[0].app_temp;
+    try {
+      const measurements = air_qualityData.results[0].measurements;
+      document.getElementById("aq_parameter").textContent =
+        measurements[0].parameter;
+      document.getElementById("aq_location").textContent =
+        air_qualityData.results[0].location;
+      document.getElementById("aq_time").textContent =
+        measurements[0].lastUpdated;
+      document.getElementById(
+        "aq_pm_ten"
+      ).textContent = `${measurements[0].value} ${measurements[0].unit}`;
+    } catch (error) {
+      console.log("Something went wrong...");
+      document.getElementById("air_quality").innerHTML =
+        "Sorry - There is no air quality information at this location.";
+    }
+  });
+} else {
+  console.log("geolocation IS NOT available");
+}
+
+const button = document.getElementById("checkin");
+button.addEventListener("click", addMarkers);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
+var markers = L.layerGroup();
+
+async function addMarkers() {
+  document.getElementById("map").style.visibility = "visible";
+  const api_url = "/weatherData";
+  const response = await fetch(api_url);
+  const json = await response.json();
+  markers.clearLayers();
+  var latlngsArray = [];
+
+  for (let i = 0; i < json.length; i++) {
+    latlngsArray.push([json[i].lat, json[i].long]);
+    const marker = L.marker([json[i].lat, json[i].long]);
+    markers.addLayer(marker);
+  }
+  markers.addTo(map);
+
+  //zoom map to fit all markers
+  var polyline = L.polyline(latlngsArray);
+  map.fitBounds(polyline.getBounds());
+}
